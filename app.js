2909a8d680a6fcdf8e1c25a4732198105c265865
Gulxayo2804 <gulxayo02@mail.ru>
2025-05-09 21:28:38 -0400

    impelimating ejs

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const rootDir = require("./util/path");
 
 app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminR = require("./routes/admin");
 const shopR = require("./routes/shop");
@@ -10,10 +13,10 @@ const shopR = require("./routes/shop");
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin',adminR);
+app.use('/admin',adminR.routes);
 app.use(shopR);
 
 app.use((req,res, next)=>{
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.status(404).render('404', { pageTitle: 'Page Not Found' , path: '/admin/add-product'});
 })
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,19 @@
 
 const express= require("express");
 const path = require("path");
+const rootDir = require("../util/path");
+const adminData = require("./admin")
 
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, '../', "views", "shop.html"));
+    console.log(adminData.products);
+    
+    res.render('shop', {
+        prods:adminData.products,
+        pageTitle: 'Shop',
+        path: '/admin/add-product'
+    });
 })
 
 module.exports = router;
