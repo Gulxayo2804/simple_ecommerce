875df67642edc10140c50acd81e3bb8262589fe0
Gulxayo2804 <gulxayo02@mail.ru>
2025-08-09 12:36:54 -0400

    add card and delete it

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -27,14 +29,14 @@ module.exports = class Product {
   }
 
   save() {
-
     getProductsFromFile(products => {
       if (this.id) {
-        const exitProductIndex = products.findIndex(prod => prod.id === this.id);
-        const updatingProducts = [...products];
-        
-        updatingProducts[exitProductIndex] = this;
-        fs.writeFile(p, JSON.stringify(updatingProducts), err => {
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
           console.log(err);
         });
       } else {
@@ -44,6 +46,18 @@ module.exports = class Product {
           console.log(err);
         });
       }
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
     });
   }
 
