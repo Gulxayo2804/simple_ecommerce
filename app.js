526eb870929beae4f0baab0e2dbf1a69e39f01a1
Gulxayo2804 <gulxayo02@mail.ru>
2025-09-03 14:00:08 -0400

    adding authenticated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -18,9 +19,10 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'my secret', resave : false, saveUninitialized: false}))
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('68b754867d960b0fdf5ae0db')
     .then(user => {
       req.user = user;
       next();
@@ -35,7 +37,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://gulkhayo:gulkhayo@cluster0.rkdaucr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .connect('mongodb+srv://gulkhayo:gulkhayo@cluster0.rkdaucr.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
