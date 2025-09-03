91617fad465fbe4921aa1bdbf0fd3115489f1241
Gulxayo2804 <gulxayo02@mail.ru>
2025-09-03 17:40:41 -0400

    adding session to the db

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://gulkhayo:gulkhayo@cluster0.rkdaucr.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,10 +26,20 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave : false, saveUninitialized: false}))
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('68b754867d960b0fdf5ae0db')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -37,7 +54,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://gulkhayo:gulkhayo@cluster0.rkdaucr.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,29 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get('Cookie').split('=')[1]==='true';
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated : isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/')
+  User.findById('68b754867d960b0fdf5ae0db')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
